fcf78cf279cec1e8707d81c38fd16e1712dd4e45
tormozz48 <tormozz48@gmail.com>
2017-10-22 10:51:53 +0300

    Написал тесты для json форматтера

--- test/task1/transformers/output/json.js
+++ test/task1/transformers/output/json.js
@@ -3,5 +3,30 @@
 const JSONOutputTransformer = require('../../../../src/task1/transformers/output/json');
 
 describe('src/task1/transformers/output/json', () => {
+    it('should properly create output in json format', () => {
+        const jsonOutputTransformer = new JSONOutputTransformer({});
+        const inputData = [
+            {foo: 'some-foo1', bar: 'some-bar1'},
+            {foo: 'some-foo2', bar: 'some-bar2'}
+        ];
 
+        assert.equal(
+            jsonOutputTransformer.run(inputData),
+            '[{"foo":"some-foo1","bar":"some-bar1"},{"foo":"some-foo2","bar":"some-bar2"}]'
+        );
+    });
+
+    it('should use custom configured "spaces" parameter', () => {
+        const jsonOutputTransformer = new JSONOutputTransformer({spaces: 2});
+        const inputData = [
+            {foo: 'some-foo1', bar: 'some-bar1'},
+            {foo: 'some-foo2', bar: 'some-bar2'}
+        ];
+
+        assert.equal(
+            jsonOutputTransformer.run(inputData),
+            '[\n  {\n    "foo": "some-foo1",\n    "bar": "some-bar1"\n  }' +
+            ',\n  {\n    "foo": "some-foo2",\n    "bar": "some-bar2"\n  }\n]'
+        );
+    });
 });
