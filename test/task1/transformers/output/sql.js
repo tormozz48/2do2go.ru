81d10b978d424a61d194f15f80a2a3d67c33ebe5
tormozz48 <tormozz48@gmail.com>
2017-10-22 11:00:49 +0300

    Добавил тесты для sql форматтера

--- test/task1/transformers/output/sql.js
+++ test/task1/transformers/output/sql.js
@@ -3,5 +3,31 @@
 const SQLOutputTransformer = require('../../../../src/task1/transformers/output/sql');
 
 describe('src/task1/transformers/output/sql', () => {
+    it('should properly create output in sql format', () => {
+        const sqlOutputTransformer = new SQLOutputTransformer({});
+        const inputData = [
+            {foo: 'some-foo1', bar: 'some-bar1'},
+            {foo: 'some-foo2', bar: 'some-bar2'}
+        ];
 
+        assert.equal(
+            sqlOutputTransformer.run(inputData),
+            'insert into "table" ("foo", "bar") values (\'some-foo1\', \'some-bar1\');\n' +
+            'insert into "table" ("foo", "bar") values (\'some-foo2\', \'some-bar2\');'
+        );
+    });
+
+    it('should use custom configured table name', () => {
+        const sqlOutputTransformer = new SQLOutputTransformer({table: 'some-custom-table'});
+        const inputData = [
+            {foo: 'some-foo1', bar: 'some-bar1'},
+            {foo: 'some-foo2', bar: 'some-bar2'}
+        ];
+
+        assert.equal(
+            sqlOutputTransformer.run(inputData),
+            'insert into "some-custom-table" ("foo", "bar") values (\'some-foo1\', \'some-bar1\');\n' +
+            'insert into "some-custom-table" ("foo", "bar") values (\'some-foo2\', \'some-bar2\');'
+        );
+    });
 });
